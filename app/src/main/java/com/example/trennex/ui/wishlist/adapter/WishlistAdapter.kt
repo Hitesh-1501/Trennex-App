65b07c95461bb10642ff00041b07fc0dccb87463
Hitesh <badgujarhitesh1501@gmail.com>
2026-04-23 22:58:49 +0530

    define a function that update the wishlist items

--- app/src/main/java/com/example/trennex/ui/wishlist/adapter/WishlistAdapter.kt
+++ app/src/main/java/com/example/trennex/ui/wishlist/adapter/WishlistAdapter.kt
@@ -1,33 +1,43 @@
 package com.example.trennex.ui.wishlist.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.trennex.R
 import com.example.trennex.databinding.ItemWishlistProductBinding
 import com.example.trennex.ui.wishlist.model.WishlistItemsModel
+import com.example.trennex.utils.CurrencyFormator
 
 class WishlistAdapter(
-    private val items : List<WishlistItemsModel>
+    private var items : List<WishlistItemsModel>,
 ): RecyclerView.Adapter<WishlistAdapter.WishlistVH>(){
 
     inner class WishlistVH(val binding: ItemWishlistProductBinding) : RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): WishlistAdapter.WishlistVH {
-        val binding = ItemWishlistProductBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WishlistVH {
+        val binding = ItemWishlistProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return WishlistVH(binding)
     }
 
     override fun onBindViewHolder(holder: WishlistAdapter.WishlistVH, position: Int) {
-        val items = items[position]
-        holder.binding.ivProduct.setImageResource(items.image)
-        holder.binding.tvTitle.text = items.title
-        holder.binding.tvPrice.text = "₹${items.price}"
+        val item = items[position]
+        holder.binding.tvTitle.text = item.title
+        holder.binding.tvPrice.text = CurrencyFormator.formatInr(item.price)
+        Glide.with(holder.binding.ivProduct)
+            .load(item.imageUrl)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .into(holder.binding.ivProduct)
     }
 
     override fun getItemCount(): Int {
        return items.size
+    }
+    @SuppressLint("NotifyDataSetChanged")
+    fun submitList(updateItems: List<WishlistItemsModel>){
+        items = updateItems
+        notifyDataSetChanged()
     }
 }
