23445a8a4d43cf6a430a7e57f7e71faa4d6ffbb9
Hitesh <badgujarhitesh1501@gmail.com>
2026-05-22 16:54:45 +0530

    Add the google services location dependency to get the current location of the user

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,4 +84,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     kapt(libs.androidx.room.compiler)
+
+    implementation(libs.play.services.location)
+
 }
