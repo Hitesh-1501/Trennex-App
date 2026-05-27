ac459fb28f4e08b3a58a54c0ccd128d22087daeb
Hitesh <badgujarhitesh1501@gmail.com>
2026-05-27 12:21:35 +0530

    initialize the google Map on AddNewAddressFragment  and  design a ui in fragment_add_new_address.xml

--- app/src/main/java/com/example/trennex/ui/location/AddNewAddressFragment.kt
+++ app/src/main/java/com/example/trennex/ui/location/AddNewAddressFragment.kt
@@ -8,13 +8,20 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
+import com.example.trennex.R
 import com.example.trennex.databinding.FragmentAddNewAddressBinding
 import com.example.trennex.databinding.SelectingLocationDialogBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class AddNewAddressFragment : Fragment() {
+class AddNewAddressFragment : Fragment(R.layout.fragment_add_new_address), OnMapReadyCallback {
     private var _binding: FragmentAddNewAddressBinding? = null
     private val binding get() = _binding!!
     private var addAddressDialog: AlertDialog? = null
+    private lateinit var googleMap: GoogleMap
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,6 +44,7 @@ class AddNewAddressFragment : Fragment() {
             .create()
         dialogBinding.farFromLocationBtn.setOnClickListener {
             addAddressDialog?.dismiss()
+            initializeMap()
         }
         dialogBinding.useCurrentLocationBtn.setOnClickListener {
             addAddressDialog?.dismiss()
@@ -50,6 +58,22 @@ class AddNewAddressFragment : Fragment() {
 
             addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
         }
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        val defaultLocation = LatLng(20.5937, 78.9629)
+        googleMap.moveCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                defaultLocation,
+                5f
+            )
+        )
+    }
+    private fun initializeMap(){
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun onDestroyView() {
