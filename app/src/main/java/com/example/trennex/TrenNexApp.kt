0faa202947dbb620f5548943a01221c7d5e6ca03
Hitesh <badgujarhitesh1501@gmail.com>
2026-07-28 20:45:35 +0530

    Global Places SDK initialization.

--- app/src/main/java/com/example/trennex/TrenNexApp.kt
+++ app/src/main/java/com/example/trennex/TrenNexApp.kt
@@ -5,11 +5,19 @@ import com.example.trennex.utils.cart.CartStore
 import com.example.trennex.utils.wishlist.CollectionStore
 import com.example.trennex.utils.wishlist.WishListStore
 
+import com.example.trennex.R
+import com.google.android.libraries.places.api.Places
+
 class TrenNexApp: Application(){
     override fun onCreate() {
         super.onCreate()
         CartStore.initialize(this)
         WishListStore.initialize(this)
         CollectionStore.initialize(this)
+        
+        val apiKey = getString(R.string.MAP_API_KEY)
+        if (apiKey.isNotBlank()) {
+            Places.initialize(this, apiKey)
+        }
     }
 }
