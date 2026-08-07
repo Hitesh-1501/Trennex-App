f2b99c39980f1718d605f0ec95fd3864707ccb3f
Hitesh <badgujarhitesh1501@gmail.com>
2026-08-07 21:04:33 +0530

    polish search toolbar background and status bar synchronization

--- app/src/main/java/com/example/trennex/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/trennex/ui/main/MainActivity.kt
@@ -235,7 +235,7 @@ class MainActivity : AppCompatActivity() {
             ToolBarType.SEARCH -> {
                 val toolbarBinding = SearchToolbarBinding.inflate(layoutInflater)
                 binding.toolbarContainer.addView(toolbarBinding.root)
-                binding.appBarLayout.setBackgroundColor("#E3EBFF".toColorInt())
+                binding.appBarLayout.setBackgroundColor( "#E3EBFF".toColorInt())
                 toolbarBinding.backArrow.setOnClickListener {
                     navController.popBackStack()
                 }
