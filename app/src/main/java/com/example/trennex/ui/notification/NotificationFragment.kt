2651707123fa26a2d40cbded45b0969f71d79808
Hitesh <badgujarhitesh1501@gmail.com>
2026-08-09 21:46:27 +0530

    implement NotificationFragment with basic back navigation

---
+++ app/src/main/java/com/example/trennex/ui/notification/NotificationFragment.kt
@@ -0,0 +1,36 @@
+package com.example.trennex.ui.notification
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.trennex.databinding.FragmentNotificationBinding
+
+class NotificationFragment : Fragment() {
+
+    private var _binding: FragmentNotificationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNotificationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnExplore.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
