7a71b7128fb1a4be3ef951a7f024c18eba5f66a1
Hitesh <badgujarhitesh1501@gmail.com>
2026-08-15 20:42:00 +0530

    migrate all subcategory fragments to fetch data from CategoryViewModel

--- app/src/main/java/com/example/trennex/ui/category/subcategories/ApplianceFragment.kt
+++ app/src/main/java/com/example/trennex/ui/category/subcategories/ApplianceFragment.kt
@@ -5,53 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.trennex.R
 import com.example.trennex.databinding.FragmentApplianceBinding
 import com.example.trennex.ui.category.adapter.ApplianceAdapter
-import com.example.trennex.ui.category.adapter.ApplianceAdapter.Companion.TYPE_BANNER
-import com.example.trennex.ui.category.adapter.ApplianceAdapter.Companion.TYPE_HEADER
 import com.example.trennex.ui.category.adapter.ApplianceAdapter.Companion.TYPE_ITEM
-import com.example.trennex.ui.category.model.ApplianceCategoryModel
+import com.example.trennex.viewmodel.category.CategoryViewModel
 
-class ApplianceFragment : Fragment(R.layout.fragment_appliance) {
+class ApplianceFragment : Fragment() {
     private var _binding : FragmentApplianceBinding? = null
     private val binding get() = _binding!!
+    
+    private val viewModel: CategoryViewModel by viewModels({ requireParentFragment() })
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentApplianceBinding.inflate(inflater,container,false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val list = bundleList()
-        val grid = GridLayoutManager(requireContext(),2)
+        val list = viewModel.getApplianceItems()
+        val grid = GridLayoutManager(requireContext(),3)
         grid.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
             override fun getSpanSize(position: Int): Int {
-                return if(list[position].Type == TYPE_ITEM) 1 else 2
+                return if(list[position].Type == TYPE_ITEM) 1 else 3
             }
         }
         binding.rvAppliance.layoutManager = grid
         binding.rvAppliance.adapter = ApplianceAdapter(list)
-    }
-
-    private fun bundleList(): List<ApplianceCategoryModel>{
-        val list = mutableListOf<ApplianceCategoryModel>()
-        list.add(ApplianceCategoryModel(TYPE_BANNER))
-        list.add(ApplianceCategoryModel(TYPE_HEADER,"Shop By Category"))
-        list.add(ApplianceCategoryModel(TYPE_ITEM,"Television",R.drawable.appliance_tv))
-        list.add(ApplianceCategoryModel(TYPE_ITEM,"Refrigerators",R.drawable.appliance_refrigerator))
-        list.add(ApplianceCategoryModel(TYPE_ITEM,"Washing\n"+ "Machines",R.drawable.appliance_washing_machine))
-        list.add(ApplianceCategoryModel(TYPE_ITEM,"Air\n" + "Conditioners",R.drawable.appliance_air_conditioner))
-        list.add(ApplianceCategoryModel(TYPE_ITEM,"Kitchen \n" + "Appliances",R.drawable.kitchen_appliances))
-        list.add(ApplianceCategoryModel(TYPE_ITEM,"Home\n" + "Appliances",R.drawable.home_appliances))
-
-        return list
     }
 
     override fun onDestroyView() {
